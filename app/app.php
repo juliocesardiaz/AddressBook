b7f8f097fc5a9d04f50c31cd78a6baa7ef05d0f0
Julio <juliocdiazramirez@gmail.com>
2015-08-07 10:03:18 -0700

    Wrote app.php and some of the twig files

---
+++ app/app.php
@@ -0,0 +1,30 @@
+<?php 
+	require_once __DIR__."/../vendor/autload.php";
+	require_once __DIR__."/../src/Contact.php";
+	
+	session_start();
+	if(empty($_SESSION[list_of_contacts])) {
+		$_SESSION[list_of_contacts] = array();
+	}
+	
+	$app = new Silex\Application();
+	$app['debug'] = true;
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+		'twig.path' => __DIR__.'/../views'
+	));
+	
+	$app->get("/", function() use ($app) {
+		return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
+	});
+	
+	$app->post("/save_contact", function() use ($app) {
+		$new_contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+		$new_contact->save();
+		return $app['twig']->render('contact_saved.html.twig', array('new_contact' => $new_contact));
+	});
+	
+	$app->post("/delete_contacts", function() use($app) {
+		Contact::deleteAll();
+		return $app['twig']->render('contacts_deleted.html.twig');
+	});
+?>
