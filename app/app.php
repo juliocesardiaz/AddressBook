1cdbec48ff6edd307fc306a07c17d46062cc20d7
Julio <juliocdiazramirez@gmail.com>
2015-08-07 10:36:56 -0700

    Added Comments

--- app/app.php
+++ app/app.php
@@ -12,17 +12,26 @@
 	$app->register(new Silex\Provider\TwigServiceProvider(), array(
 		'twig.path' => __DIR__.'/../views'
 	));
-	
+	/*
+	Renders the homepage and sends all intances of contacts to get malipulated by Twig
+	*/
 	$app->get("/", function() use ($app) {
 		return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
 	});
 	
+	/*
+	Creates a new instance of contact initialized with the variables input by the user in the homepage
+	Renders the contact_saved page and sends the new contact created to be diplayed on the new page
+	*/
 	$app->post("/save_contact", function() use ($app) {
 		$new_contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
 		$new_contact->save();
 		return $app['twig']->render('contact_saved.html.twig', array('new_contact' => $new_contact));
 	});
 	
+	/*
+	Renders the delete_contacts page and deletes all instances of Contact
+	*/
 	$app->post("/delete_contacts", function() use($app) {
 		Contact::deleteAll();
 		return $app['twig']->render('contacts_deleted.html.twig');
--- src/Contact.php
+++ src/Contact.php
@@ -1,17 +1,28 @@
 <?php 
+	/*
+	Contact Class
+	*/
 	class Contact
 	{
 		private $name;
 		private $phone;
 		private $address;
 		
+		/* Constructor for the Contact Class
+			Takes three variables that set the name, phone and
+			address of the contact
+		*/
 		function __construct($name, $phone, $address)
 		{
 			$this->name = $name;
 			$this->phone = $phone;
 			$this->address = $address;
 		}
-		
+		/*
+		Getters and Setters for each of the private variables 
+		of the contact class. Each setter takes one variable and
+		sets it to the private variable it is setting to 
+		*/
 		function setName($new_name)
 		{
 			$this->name = $new_name;
@@ -41,17 +52,26 @@
 		{
 			return $this->address;
 		}
-		
+		/*
+		Saves  a new instance of Contact into the array of Contacts created
+		by each use session.
+		*/
 		function save()
 		{
 			array_push($_SESSION['list_of_contacts'], $this);
 		}
 		
+		/*
+		Returns all the intances of Contacts saved in the users cookie
+		*/
 		static function getAll()
 		{
 			return $_SESSION['list_of_contacts'];
 		}
 		
+		/*
+		Deletes all the intances of Contacts saved in the users cookie
+		*/
 		static function deleteAll()
 		{
 			$_SESSION['list_of_contacts'] = array();
